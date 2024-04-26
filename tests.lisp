68fd4c6efef91e70b8ec44121c33ea63fa584bff
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-04-26 11:21:07 +0200

    Added tests for sum-list

--- tests.lisp
+++ tests.lisp
@@ -10,7 +10,14 @@
 (def-suite* :tests)
 
 (test test-fact
-  (is (= 1 (fact 0)))
-  (is (= 2 (fact 2)))
-  (is (= 24 (fact 4)))
-  (is (= 3628800 (fact 10))))
+      (is (= 1 (fact 0)))
+      (is (= 2 (fact 2)))
+      (is (= 24 (fact 4)))
+      (is (= 3628800 (fact 10))))
+
+(test test-sum-list
+      (is (= 0 (sum-list '())))
+      (is (= 6 (sum-list '(1 2 3))))
+      (is (= 2 (sum-list '(1 1))))
+      (is (= 4 (sum-list '(3 1))))
+      (is (= 10 (sum-list '(1 1 1 1 4 2)))))
