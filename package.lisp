e812b4fd5acdb13986ed4c4c3075fd82f4771d89
Jonas Dornieden <jonas.dornieden@student.auc.nl>
2024-05-05 16:22:56 +0200

    function simplify-arithmetic and helpers, and added to package

--- package.lisp
+++ package.lisp
@@ -10,4 +10,5 @@
            :node-left :search-bt :search-bst :path-ton :bt-path-ton :left
            :right :t :cons-to-end :follow-path :bt-ltor :combine-lists
            :bt-depth-first :matom :msxp :matom-p :make-matom :make-msxp :msxp-p
-           :get-strings :get-sum-sx))
+           :get-strings :get-sum-sx :check-arithmetic :check-symbol
+           :evaluate-arithmetic :simplify-arithmetic))
